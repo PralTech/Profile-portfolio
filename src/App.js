695bc5fba059b1f0ac9420e7dd72e32a70d7e98a
Pr@lTech <pralhadtalmale@gmail.com>
2022-12-02 09:37:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
+import Services from './Components/Services/Services'
+import Work from './Components/Works/work'
+import Portfolio from './Components/Portfolio/Portfolio'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/Home' element={<Home />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/work' element={<Work />} />
+      <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/testimonials' element={<Testimonials />} />
+      <Route path='/contact' element={<Contact />} />
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
+
+  
 }
 
 export default App;
